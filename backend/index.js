6ed560bbeec4a53dcb7f85fe6a198981cea6957e
Aayush <aayush.130702@gmail.com>
2023-03-03 09:25:27 +0530

    Updated

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
 const cors = require("cors");
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
   next();
 });
 app.get("/api/data", (req, res) => {
-  res.json({ message: "Hello World!" });
+  res.json({ message: "Hello Everyone" });
 });
 app.listen(process.env.PORT, () => {
   console.log("Server Up and Running");
